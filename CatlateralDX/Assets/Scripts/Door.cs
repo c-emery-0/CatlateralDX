d91c6208158e6d54bf1fafb042cd2fad63e15898
celeste emery <beecroncher@gmail.com>
2024-04-28 23:43:00 -0500

    doors stuff is so fucked

--- CatlateralDX/Assets/Scripts/Door.cs
+++ CatlateralDX/Assets/Scripts/Door.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] GameObject[] objectsBehindDoor;
+    [SerializeField] List<GameObject> objectsBehindDoor;
     [SerializeField] Sprite doorClose, doorOpen;
 
     private SpriteRenderer spriteRenderer;
     
     private bool isOpen = true;
+
+    private int fuck;
 
     // Start is called before the first frame update
     void Start()
@@ -35,16 +37,36 @@ public class Door : MonoBehaviour
         foreach (GameObject obj in objectsBehindDoor) {
             Collider2D[] colliders = obj.GetComponents<Collider2D>();
             foreach (Collider2D coll in colliders) {
-                coll.enabled = isOpen;
+                coll.forceReceiveLayers = (isOpen) ? LayerMask.NameToLayer("Everything") : LayerMask.GetMask("Nothing");
+                coll.forceSendLayers = (isOpen) ? LayerMask.NameToLayer("Everything") : LayerMask.GetMask("Nothing");
+                
             }
 
             obj.GetComponent<Transform>().position = new Vector3(obj.GetComponent<Transform>().position.x, 
                                 obj.GetComponent<Transform>().position.y, (isOpen) ? -2 : 0); //if z=-2, then objects are in front of the door
 
             try {
+
                 obj.GetComponent<Rigidbody2D>().isKinematic = !isOpen;
             } catch {}
         }
 
+    }
+
+    private void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("OneWayPlatform") || collision.gameObject.CompareTag("Player")) return;
+
+        Collider2D obj = collision.gameObject.GetComponent<Collider2D>();
+        Collider2D doorCollider = GetComponent<Collider2D>();
+        
+        if (doorCollider.bounds.Contains(obj.bounds.min) 
+                && ! objectsBehindDoor.Contains(collision.gameObject)) {
+            objectsBehindDoor.Add(collision.gameObject);
+        }
+        else if ((!doorCollider.bounds.Contains(obj.bounds.min) 
+                || !doorCollider.bounds.Contains(obj.bounds.max))
+                && objectsBehindDoor.Contains(collision.gameObject)) {
+            objectsBehindDoor.Remove(collision.gameObject);
+        } 
     }
 }
